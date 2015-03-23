c8903dedf8a4427deb8ca77e7d85d5b906c302ee
chadbartel <therealraccoonchad@gmail.com>
2015-03-23 15:50:23 -0700

    Added additional example.

--- tkinter-practice.py
+++ tkinter-practice.py
@@ -1,5 +1,6 @@
 __author__ = 'v-chbart'
 
+'''
 from tkinter import *
 
 root = Tk()
@@ -8,3 +9,30 @@ w = Label(root, text="Hello, world!")
 w.pack()
 
 root.mainloop()
+'''
+
+from tkinter import *
+
+class App:
+
+    def __init__(self, master):
+        frame = Frame(master)
+        frame.pack()
+
+        self.button = Button(
+            frame, text="QUIT", fg="red", command=frame.quit
+        )
+        self.button.pack(side=LEFT)
+
+        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
+        self.hi_there.pack(side=LEFT)
+
+    def say_hi(self):
+        print("Hi there, everyone!")
+
+root = Tk()
+
+app = App(root)
+
+root.mainloop()
+root.destroy()  # optional
